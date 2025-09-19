2e0b307b257a880ed37de328b5e3e3f19bb1dd15
hed-dsanchez-dev <hed-dsanchez@smu.edu.ph>
2025-09-19 09:57:51 +0800

    Add files via upload

---
+++ SanchezFTEprob1/SanchezFTEprob1/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txt = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(230, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "select beverage"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"espresso", "latte", "capuccino", "americano", "mocha"})
+        Me.ComboBox1.Location = New System.Drawing.Point(319, 77)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(355, 107)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(121, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "apply loyalty dicount"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(340, 130)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "place order"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AutoSize = True
+        Me.TextBox1.Location = New System.Drawing.Point(230, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(86, 13)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "customer's name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(244, 104)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "quantity"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(375, 303)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(0, 13)
+        Me.Label7.TabIndex = 11
+        '
+        'txt
+        '
+        Me.txt.Location = New System.Drawing.Point(294, 105)
+        Me.txt.Name = "txt"
+        Me.txt.Size = New System.Drawing.Size(54, 20)
+        Me.txt.TabIndex = 12
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(352, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "final total"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(340, 51)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txt As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ SanchezFTEprob1/SanchezFTEprob1/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox1.Text.Trim() = "" Then
+            MessageBox.Show("Customer's name.")
+            Exit Sub
+        End If
+
+        If ComboBox1.SelectedIndex = -1 Then
+            MessageBox.Show(" Beverage.")
+            Exit Sub
+        End If
+        If Not IsNumeric(txt.Text) OrElse CInt(txt.Text) <= 0 Then
+            MessageBox.Show(" Quantity.")
+            Exit Sub
+        End If
+
+
+        Dim price As Decimal = 0
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "Espresso" : price = 120
+            Case "Latte" : price = 150
+            Case "Cappuccino" : price = 160
+            Case "Americano" : price = 130
+            Case "Mocha" : price = 170
+        End Select
+
+        Dim qty As Integer = CInt(txt.Text)
+        Dim subtotal As Decimal = price * qty
+        Dim discount As Decimal = 0
+        If CheckBox1.Checked Then
+            discount = subtotal * 0.1D
+        End If
+        Dim finalTotal As Decimal = subtotal - discount
+
+
+        Label7.Text = "Subtotal: " & subtotal.ToString("C") & vbCrLf &
+                      "Discount: " & discount.ToString("C") & vbCrLf &
+                      "Final Total: " & finalTotal.ToString("C")
+
+
+        MessageBox.Show("Order " & TextBox1.Text & " has been placed." & vbCrLf &
+                        "Final Total:" & finalTotal.ToString("C"))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
